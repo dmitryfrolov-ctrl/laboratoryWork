d551a3a1c85e61dbb3723c6b8f0c4d1fe74ffee6
Дмитрий Фролов <bk.dmitry.frolov@gmail.com>
2025-12-26 01:20:08 +0300

    Лабораторная 4

---
+++ lab_4_1.py
@@ -0,0 +1,26 @@
+class UserAccount:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.__password = password  # Приватный атрибут
+
+    def check_password(self, password):
+        return self.__password == password
+
+    def set_password(self, current_password, new_password):
+        if not self.check_password(current_password):
+            print("Текущий пароль неправильный")
+            return False
+        if self.check_password(new_password):
+            print("Пароль должен отличаться")
+            return False
+        self.__password = new_password
+        print("все хорошо")
+        return True
+
+
+if __name__ == "__main__":
+    user = UserAccount("dima", "dima@example.com", "123")
+
+    user.set_password(input("new_password"),input("current_password"))
+
