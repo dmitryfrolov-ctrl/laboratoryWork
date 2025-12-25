874a95d77417f37ccd37f693e9a058d9bf27dca6
Дмитрий Фролов <bk.dmitry.frolov@gmail.com>
2025-12-25 20:31:14 +0300

    Лабораторная 3

---
+++ lab_3.py
@@ -0,0 +1,24 @@
+class book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def get_info(self):
+        return f"Название книги: {self.title}, Автор: {self.author}, Год издания: {self.year}"
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_radius(self):
+        return self.radius
+
+    def set_radius(self, new_radius):
+        self.radius = new_radius
+
+
+circle = Circle(10)
+circle.set_radius(11)
+print(f"Радиус: {circle.get_radius()}")
